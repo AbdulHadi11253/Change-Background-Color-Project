0bfb297a42ccd47bfa63b685784b9b1c5a187bd9
Abdul Hadi <abdulhadi91478@gmail.com>
2023-01-27 23:38:44 +0500

    Project Completed Random Background generator

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function changebackground(){
-document.getElementById("background").style.background = "black";
+    const changeColor = Math.floor(Math.random()*16777215).toString(16);//tostring function generates us a hexadecimal value:
+document.getElementById("background").style.background = "#"+ changeColor;
+console.log(changeColor);
 }
 
